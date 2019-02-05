9ed96631a8d4fa5d3d96af73f765f888425d1b28
J Fern <jfern20@gmail.com>
2019-02-05 08:06:29 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,8 +14,8 @@ class Patient
    end 
  
   def new_appointment(doctor, date)
-    appointment = Appointment.new(self, doctor, date )
-    self.appointments.last 
+    Appointment.new(self, doctor, date )
+ 
   end 
  
  def appointments
